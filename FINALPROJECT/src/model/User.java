7751424548ca4430c4ee7aa1dec1a4f178f9aaca
Jan Kirby <jvmanzanero@up.edu.ph>
2026-04-21 17:31:41 +0800

    Update User.java

--- FINALPROJECT/src/model/User.java
+++ FINALPROJECT/src/model/User.java
@@ -1,5 +1,20 @@
 package model;
 
 public abstract class User {
+    private String username;
+    private String password; 
+    private String fullName;
+    private String role;
 
+    public User(String username, String password, String fullName, String role){
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.role = role;
+    }
+
+    public String getUsername(){return username;}
+    public String getPassword(){return password;}
+    public String getFullName(){return fullName;}
+    public String getRole(){return role;}
 }

229e262ae8e3036d284c8d209dd3e881c3dc42f0
Jan Kirby <jvmanzanero@up.edu.ph>
2026-04-21 17:43:21 +0800

    Update InventoryItem.java

--- FINALPROJECT/src/model/InventoryItem.java
+++ FINALPROJECT/src/model/InventoryItem.java
@@ -1,5 +1,23 @@
 package model;
 
 public class InventoryItem {
-	
+		private String itemName;
+		private String description;
+		private int stocks;
+		private int price;
+		private int ingredientUsage;
+
+		public InnventoryItem(String itemName, String description, int stocks, int price){
+				this.itemName = itemName;
+				this.description = description;
+				this.stocks = stocks;
+				this.price = price;
+				this.ingredientUsage = 0;
+		}
+
+		public String getItemName(){return itemName;}
+		public String description(){return description;}
+		public int getStocks(){return stocks;}
+		public int getPrice(){return price;}
+		public int getIngredientsUsage(){return ingredientsUsage;}
 }

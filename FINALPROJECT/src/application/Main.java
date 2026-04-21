800b1832d900d04c8deb5b985bc7b6f1c46d8f69
kpcaballero14 <caballerokarmelapaula@gmail.com>
2026-04-21 17:45:27 +0800

    Enhanced Main.java with media and background features
    Added media playback and background image functionality.

--- FINALPROJECT/src/application/Main.java
+++ FINALPROJECT/src/application/Main.java
@@ -4,6 +4,11 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 
 public class Main extends Application {
@@ -17,6 +22,29 @@ public class Main extends Application {
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
+		}
+		String musicFile = "shop_music.mp3";     // For example
+    	Media sound = new Media(new File(musicFile).toURI().toString());
+    	MediaPlayer mediaPlayer = new MediaPlayer(sound);
+    	mediaPlayer.play();
+		StackPane root = new StackPane();
+		primaryStage.setScene(new Scene(root, 800, 450));
+		primaryStage.show();
+
+		Pane root = new Pane();
+
+		try {
+			Image backgroundImage = new Image("file:background.jpg"); // placeholder
+			BackgroundImage bgImage = new BackgroundImage(
+				backgroundImage,
+                    BackgroundRepeat.NO_REPEAT, // X repeat
+                    BackgroundRepeat.NO_REPEAT, // Y repeat
+                    BackgroundPosition.CENTER, // Position
+					new BackgroundSize(
+						BackgroundSize.AUTO, BackgroundSize.AUTO,
+						false, false, true, false
+					)
+			);
 		}
 	}
 	

0e57e9d27367bad5f3e9dbef6b33e20e65851b05
Jan Kirby <jvmanzanero@up.edu.ph>
2026-04-21 17:46:24 +0800

    Update MenuItem.java

--- FINALPROJECT/src/model/MenuItem.java
+++ FINALPROJECT/src/model/MenuItem.java
@@ -1,20 +1,5 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Menu {
-    private List<MenuItem> items;
-
-    public Menu() {
-        items = new ArrayList<>();
-    }
-
-    public void addMenuItem(MenuItem item) {
-        items.add(item);
-    }
-
-    public List<MenuItem> getItems() {
-        return items;
-    }
+public class MenuItem {
+  
 }

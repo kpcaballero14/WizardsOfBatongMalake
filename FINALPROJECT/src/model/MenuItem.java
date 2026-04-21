d50a38317fc2973feb2efa5071d4a15fc3cad1ee
Jan Kirby <jvmanzanero@up.edu.ph>
2026-04-21 17:49:36 +0800

    Update MenuItem.java

--- FINALPROJECT/src/model/MenuItem.java
+++ FINALPROJECT/src/model/MenuItem.java
@@ -1,5 +1,17 @@
 package model;
 
 public class MenuItem {
-  
+    private String itemName;
+    private String description;
+    private int price;
+
+    public MenuItem(String itemName, String description, int price){
+        this.itemName = itemName;
+        this.description = description;
+        this.price = price;
+    }
+
+  public String getItemName(){return itemName;}
+  public String getDescription(){return description;}
+  public int getPrice(){return price;}
 }

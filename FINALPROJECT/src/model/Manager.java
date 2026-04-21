f5a22d963295f576ff1bd5dd41e27b2e11be901b
Jan Kirby <jvmanzanero@up.edu.ph>
2026-04-21 17:33:11 +0800

    Update Manager.java

--- FINALPROJECT/src/model/Manager.java
+++ FINALPROJECT/src/model/Manager.java
@@ -1,5 +1,7 @@
 package model;
 
 public class Manager extends User {
-	
+    public Manager(String username, String password, String fullName) {
+        super(username, password, fullName, "Manager");
+    }
 }

1c8158a4f135c9c711ae1759cc8fd0647d878c07
Jan Kirby <jvmanzanero@up.edu.ph>
2026-04-21 17:32:33 +0800

    Update Cashier.java

--- FINALPROJECT/src/model/Cashier.java
+++ FINALPROJECT/src/model/Cashier.java
@@ -1,5 +1,7 @@
 package model;
 
 public class Cashier extends User {
-	
+    public Cashier(String username, String password, String fullName) {
+        super(username, password, fullName, "Cashier");
+    }
 }

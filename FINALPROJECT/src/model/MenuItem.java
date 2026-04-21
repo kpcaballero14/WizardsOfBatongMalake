4980d9224a0115bfd024af0892249dde0e68fc40
Jan Kirby <jvmanzanero@up.edu.ph>
2026-04-21 17:59:07 +0800

    Update MenuItem.java

--- FINALPROJECT/src/model/MenuItem.java
+++ FINALPROJECT/src/model/MenuItem.java
@@ -4,14 +4,17 @@ public class MenuItem {
     private String itemName;
     private String description;
     private int price;
+    private double profitPercentage;
 
     public MenuItem(String itemName, String description, int price){
         this.itemName = itemName;
         this.description = description;
         this.price = price;
+        this.profitPercentage = ((price-(price-500))/price)*100;
     }
 
   public String getItemName(){return itemName;}
   public String getDescription(){return description;}
   public int getPrice(){return price;}
+  public double getProfitPercentage(){return profitPercentage;}
 }

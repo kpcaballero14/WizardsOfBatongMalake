4c04f9b56954b75a17e9f4ea22eb690043ecf428
Jan Kirby <jvmanzanero@up.edu.ph>
2026-04-21 17:35:34 +0800

    Update Inventory.java

--- FINALPROJECT/src/model/Inventory.java
+++ FINALPROJECT/src/model/Inventory.java
@@ -1,5 +1,20 @@
 package model;
 
-public class Inventory {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Inventory {
+    private List<InventoryItem> items;
+
+    public Inventory() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(InventoryItem item) {
+        items.add(item);
+    }
+
+    public List<InventoryItem> getItems() {
+        return items;
+    }
 }
